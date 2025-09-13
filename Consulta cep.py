564ad3343b24364850fab4eae1fe4ab0c5646d56
Vinicius Fiscus <vinicius.fiscus@icloud.com>
2025-09-12 23:37:47 -0300

    Add files via upload

---
+++ Consulta cep.py
@@ -0,0 +1,18 @@
+import requests
+
+def consulta_cep(cep):
+    response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+    data = response.json()
+    if 'erro' not in data:
+        print('CEP: ', data['cep'])
+        print('Logradouro: ', data['logradouro'])
+        print('Complemento: ', data['complemento'])
+        print('Bairro: ', data['bairro'])
+        print('Localidade: ', data['localidade'])
+        print('UF: ', data['uf'])
+        print('IBGE: ', data['ibge'])
+        print('GIA: ', data['gia'])
+    else:
+        print('CEP inválido')
+
+consulta_cep('21060004')  
